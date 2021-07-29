f60f2f7b4728b75d2a3eb9df61a985c33084f263
jorgealcalah <jorgealcala1986@gmail.com>
2021-07-28 22:53:24 -0500

    h bridge added

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <Arduino.h>
 #define vx 15
 #define vy 2
+#define m1a 27
+#define m1b 26
+#define m2a 12
+#define m2b 14
 
 void setup() {
   pinMode(vx,INPUT);
   pinMode(vy,INPUT);
-  Serial.begin(9600);
-  Serial.println("Hola");
+  pinMode(m1a,OUTPUT);
+  pinMode(m1b,OUTPUT);
+  pinMode(m2a,OUTPUT);
+  pinMode(m2b,OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
@@ -14,5 +21,9 @@ void loop() {
   Serial.print(analogRead(vx));
   Serial.print(" valor en y: ");
   Serial.println(analogRead(vy));
-  
+  digitalWrite(m1a,HIGH);
+  digitalWrite(m1b,LOW);
+  digitalWrite(m2a,HIGH);
+  digitalWrite(m2b,LOW);
+
 }
