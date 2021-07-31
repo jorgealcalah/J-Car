f08070e65ec969a08d9d8760e82b9bd0a243ad9c
jorgealcalah <jorgealcala1986@gmail.com>
2021-07-30 23:38:24 -0500

    pwm added (improvement pending)

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
 #include <Arduino.h>
+#include <analogWrite.h>
 #define vx 15 //pines de joystick
 #define vy 2  //pedal
-
 #define m1a 27 //pines de control de motor
 #define m1b 26
 #define m2a 12
 #define m2b 14
-
 #define pwm1 18 //pines de PWM
 #define pwm2 19
-
 uint16_t velMot1;
 uint16_t velMot2;
+
 void setup()
 {
   Serial.begin(9600);
@@ -25,13 +24,8 @@ void setup()
   pinMode(m1b, OUTPUT);
   pinMode(m2a, OUTPUT);
   pinMode(m2b, OUTPUT);
-
-  /*
-  digitalWrite(m1a,HIGH);
-  digitalWrite(m1b,LOW);
-  digitalWrite(m2a,LOW);
-  digitalWrite(m2b,HIGH);
-  */
+  pinMode(pwm1,OUTPUT);
+  pinMode(pwm2,OUTPUT);
 }
 
 void loop()
@@ -52,8 +46,8 @@ void loop()
     digitalWrite(m1b, LOW);
     digitalWrite(m2a, LOW);
     digitalWrite(m2b, HIGH);
-    velMot1 = map(pedal, 1868, 0, 0, 255);
-    velMot2 = map(pedal, 1868, 0, 0, 255);
+    velMot1 = map(pedal, 1844, 0, 0, 255);
+    velMot2 = map(pedal, 1844, 0, 0, 255);
   }
 
   else if (pedal > 1890) //joystick hacia atrás
@@ -62,15 +56,19 @@ void loop()
     digitalWrite(m1b, HIGH);
     digitalWrite(m2a, HIGH);
     digitalWrite(m2b, LOW);
-    velMot1 = map(pedal, 1880, 4095, 0, 255);
-    velMot2 = map(pedal, 1880, 4095, 0, 255);
+    velMot1 = map(pedal, 1890, 4095, 0, 255);
+    velMot2 = map(pedal, 1890, 4095, 0, 255);
   }
+  
+  analogWrite(pwm1,velMot1);
+  analogWrite(pwm2,velMot2);
+  /*
   Serial.print("valor en x: ");
   Serial.print(analogRead(vx));
   Serial.print(" valor en y: ");
   Serial.print(pedal);
   Serial.print(" la señal de pwm tiene un valor de: ");
-  Serial.println(""); 
+  Serial.println(""); */
   /* 
   analogWrite(pwm1,velMot1);
   analogWrite(pwm2,velMot2);
