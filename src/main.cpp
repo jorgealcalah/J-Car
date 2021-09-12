594e9afa7f296568135f59319d3ce2a7ff1ba56e
jorgealcalah <jorgealcala1986@gmail.com>
2021-09-12 18:59:38 -0500

    codigo de sensor ultrasonico en main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <I_D.h>
 //pines banneados temporalmente STRAPPING: 14, 15, 10, 13, 16
 
@@ -17,4 +18,37 @@ void loop()
   volante.steerWheel();
   claxon.activateClaxon();
   //Serial.println(analogRead(39)); 
+}
+
+*/
+#include <Arduino.h>
+#include <ESP32Tone.h>
+#define BuzzerReverse 43
+#define echo 23
+#define trig 45
+
+void setup()
+{
+  pinMode(BuzzerReverse,OUTPUT);
+  pinMode(echo,INPUT);
+  pinMode(trig,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(trig, HIGH);     // generacion del pulso a enviar
+  delay(1);       // al pin conectado al trigger
+  digitalWrite(trig, LOW);    // del sensor
+  
+  uint16_t DURACION = pulseIn(echo, HIGH);  // con funcion pulseIn se espera un pulso
+            // alto en Echo
+  uint16_t DISTANCIA = DURACION / 58.2;    // distancia medida en centimetros
+  Serial.println(DISTANCIA);    // envio de valor de distancia por monitor serial
+  delay(200);       // demora entre datos
+
+  if (DISTANCIA <= 40 && DISTANCIA >= 0){ // si distancia entre 0 y 20 cms.
+    tone(BuzzerReverse, 262); //un cuarto de la nota C4.      // enciende LED
+    delay(DISTANCIA * 10);      // demora proporcional a la distancia
+    digitalWrite(BuzzerReverse, LOW);     // apaga LED
+    }
 }
