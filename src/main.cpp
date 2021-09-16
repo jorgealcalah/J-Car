4bc08a438b63bef16c531d636629f525b7b73029
jorgealcalah <jorgealcala1986@gmail.com>
2021-09-16 18:35:48 -0500

    ultrasonic sensor works but not with delay

--- src/main.cpp
+++ src/main.cpp
@@ -23,11 +23,12 @@ void loop()
 */
 #include <Arduino.h>
 #include <ESP32Tone.h>
-#define BuzzerReverse 43
-#define echo 23
-#define trig 45
-#define delayPulse 10 //CAMBIAR - tiempo para sensor ultrasónico
-unsigned long tiempoAnterior = 0;
+#define BuzzerReverse 13
+#define echo 34
+#define trig 1
+#define delayPulseTrig 0.010
+unsigned long tiempoAnteriorTrig = 0;
+unsigned long previousTimeUltSen = 0;
 bool ultrasonicState = LOW;
 
 void setup()
@@ -35,14 +36,15 @@ void setup()
   pinMode(BuzzerReverse, OUTPUT);
   pinMode(echo, INPUT);
   pinMode(trig, OUTPUT);
+  //Serial.begin(9600);
 }
 
 void loop()
 {
-  unsigned long tiempoActual = micros();
-  if (tiempoActual - tiempoAnterior >= delayPulse)
+  unsigned long tiempoActualTrig = millis();
+  if (tiempoActualTrig - tiempoAnteriorTrig >= delayPulseTrig)
   {
-    tiempoAnterior = tiempoActual;
+    tiempoAnteriorTrig = tiempoActualTrig;
     if (ultrasonicState == LOW)
     {
       ultrasonicState = HIGH;
@@ -51,16 +53,17 @@ void loop()
     {
       ultrasonicState = LOW;
     }
-    digitalWrite(trig, ultrasonicState);
+    digitalWrite(trig, ultrasonicState); //envía un pulso de 10us a trigger
   }
 
-  uint16_t DURACION = pulseIn(echo, HIGH); // con funcion pulseIn se espera un pulso
-                                           // alto en Echo
-  uint16_t DISTANCIA = DURACION / 58.2;    // distancia medida en centimetros
+  unsigned long DURACION = pulseIn(echo, HIGH); // con funcion pulseIn se espera un pulso
+  uint16_t DISTANCIA = DURACION * 0.034 / 2;    // distancia en cm
 
   if (DISTANCIA <= 40 && DISTANCIA >= 0)
-  {                           // si distancia entre 0 y 20 cms.
-    tone(BuzzerReverse, 262); //un cuarto de la nota C4.
+  {
+    uint8_t datoDelay = map(DISTANCIA, 40, 0, 10, 0); //corregir
+    unsigned long tiempoActualSensor = millis();
+    tone(BuzzerReverse, 262);
   }
   else
   {
